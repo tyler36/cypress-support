b22f8e342f70d91d73d0814f8654dd4c143353c0
tyler36 <7234392+tyler36@users.noreply.github.com>
2024-12-11 12:56:25 +0900

    move form-related steps

--- step_definitions/interactions/form.js
+++ step_definitions/interactions/form.js
@@ -2,3 +2,6 @@ import { When } from '@badeball/cypress-cucumber-preprocessor'
 
 When( 'I type {string} into {string}', cy.typeStringInto )
 When( '{string}に{string}と入力します', ( name, query ) => cy.typeStringInto( query, name ) )
+
+When( 'I submit the form', cy.submit )
+When( 'フォームを送信します', cy.submit )
