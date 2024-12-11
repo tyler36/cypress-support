4c23f8b03c6c3575868baff94ec5eb36cf43dd2b
tyler36 <7234392+tyler36@users.noreply.github.com>
2024-12-11 12:46:24 +0900

    change step signature

--- step_definitions/browser/viewport.js
+++ step_definitions/browser/viewport.js
@@ -1,7 +1,7 @@
 import { Given } from '@badeball/cypress-cucumber-preprocessor'
 
-Given( 'screen is {int}x{int}', cy.viewport )
+Given( 'the screen is {int}x{int}', cy.viewport )
 Given( '画面は{int}x{int}です', cy.viewport )
 
-Given( 'device is {string}', cy.viewport )
+Given( 'the device is {string}', cy.viewport )
 Given( 'デバイスは{string}です', cy.viewport )
--- step_definitions/interactions/wait.js
+++ step_definitions/interactions/wait.js
@@ -1,6 +1,6 @@
 import { Then, When } from '@badeball/cypress-cucumber-preprocessor'
 
-When( 'wait {string}', ms => cy.wait( parseInt( ms ) ) )
+When( 'I wait {string}', ms => cy.wait( parseInt( ms ) ) )
 When( '{string}待つ', ms => cy.wait( parseInt( ms ) ) )
 
 Then( 'wait {string} ms', ms => cy.wait( parseInt( ms ) ) )
